4b34b74797d2cab7b15f92c45052dba178999421
fengluodb <woaibiancheng31@gmail.com>
2023-07-16 21:28:33 +0800

    fix: dockerfile and helm

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -4,5 +4,5 @@ const (
 	ContainerName           = "kubebench"
 	PgbenchImage            = "postgres:latest"
 	SysbenchImage           = "registry.cn-hangzhou.aliyuncs.com/apecloud/customsuites:latest"
-	PrometheusExporterImage = "apecloud/kubebench:0.0.1"
+	PrometheusExporterImage = "registry.cn-hangzhou.aliyuncs.com/apecloud/kubebench:0.0.1"
 )

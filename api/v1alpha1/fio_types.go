69a909e4501b770b13c8ab8cdcdf3f7880f4a52c
dingben <woaibiancheng31@gmail.com>
2023-09-22 10:21:02 +0800

    feat: support fio (#38)

---
+++ api/v1alpha1/fio_types.go
@@ -0,0 +1,133 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// FioSpec defines the desired state of Fio
+// Reference https://fio.readthedocs.io/en/latest/fio_doc.html
+type FioSpec struct {
+	// The total size of file I/O for each thread of the test.
+	// Please refer to https://fio.readthedocs.io/en/latest/fio_doc.html#i-o-size
+	// +kubebuilder:validation:Pattern=^[0-9]+[kKmMgG]?$
+	// +kubebuilder:default="1G"
+	// +optional
+	Size string `json:"size,omitempty"`
+
+	// The block size to use for the test.
+	// Please refer to https://fio.readthedocs.io/en/latest/fio_doc.html#block-size
+	// +kubebuilder:validation:Pattern=^[0-9]+[kKmMgG]?$
+	// +kubebuilder:default="4k"
+	// +optional
+	Bs string `json:"bs,omitempty"`
+
+	// The number of threads to use for the test.
+	// Please refer to https://fio.readthedocs.io/en/latest/fio_doc.html#job-description
+	// +kubebuilder:validation:MinItems=1
+	// +kubebuilder:default={1}
+	// +optional
+	Numjobs []int `json:"numjobs,omitempty"`
+
+	// Limit runtime. The test will run until it completes the configured I/O workload
+	// or until it has run for this specified amount of time, whichever occurs first.
+	// Please refer to https://fio.readthedocs.io/en/latest/fio_doc.html##time-related-parameters
+	// +kubebuilder:validation:Minimum=0
+	// +optional
+	RunTime int `json:"runtime,omitempty"`
+
+	// Number of I/O units to keep in flight against the file.
+	// Please refer to https://fio.readthedocs.io/en/latest/fio_doc.html#i-o-depth
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:default=1
+	// +optional
+	Iodepth int `json:"iodepth,omitempty"`
+
+	// The I/O engine to use.
+	// Please refer to https://fio.readthedocs.io/en/latest/fio_doc.html#cmdoption-arg-ioengine
+	// +kubebuilder:default=psync
+	// +optional
+	IoEngine string `json:"ioengine,omitempty"`
+
+	// Whether to use non-buffered I/O.
+	// Please refer to https://fio.readthedocs.io/en/latest/fio_doc.html#i-o-type
+	// +kubebuilder:default=true
+	// +optional
+	Direct bool `json:"direct,omitempty"`
+
+	// The type of I/O pattern to use.
+	// Please refer to https://fio.readthedocs.io/en/latest/fio_doc.html#i-o-type
+	// +kubebuilder:validation:MinItems=1
+	// +kubebuilder:default={read}
+	// +optional
+	Rws []string `json:"rws,omitempty"`
+
+	// The other fio run command flags to use for benchmark
+	// +optional
+	ExtraArgs []string `json:"extraArgs,omitempty"`
+
+	// If specified, the pod's tolerations.
+	// +optional
+	Tolerations []corev1.Toleration `json:"tolerations,omitempty" protobuf:"bytes,22,opt,name=tolerations"`
+}
+
+// FioStatus defines the observed state of Fio
+type FioStatus struct {
+	// Phase is the current state of the test. Valid values are Disabled, Enabled, Failed, Enabling, Disabling.
+	// +kubebuilder:validation:Enum={Pending,Running,Complete,Failed}
+	Phase BenchmarkPhase `json:"phase,omitempty"`
+
+	// completions is the completed/total number of pgbench runs
+	Completions string `json:"completions,omitempty"`
+
+	// succeeded is the number of successful pgbench runs
+	Succeeded int `json:"succeeded,omitempty"`
+
+	// total is the number of pgbench runs
+	Total int `json:"total,omitempty"`
+
+	// Describes the current state of benchmark conditions.
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Fio is the Schema for the fios API
+type Fio struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   FioSpec   `json:"spec,omitempty"`
+	Status FioStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// FioList contains a list of Fio
+type FioList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Fio `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Fio{}, &FioList{})
+}
---
+++ internal/controller/fio_controller.go
@@ -0,0 +1,138 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	benchmarkv1alpha1 "github.com/apecloud/kubebench/api/v1alpha1"
+	intctrlutil "github.com/apecloud/kubebench/internal/controllerutil"
+	"github.com/apecloud/kubebench/internal/utils"
+)
+
+// FioReconciler reconciles a Fio object
+type FioReconciler struct {
+	client.Client
+	Scheme     *runtime.Scheme
+	RestConfig *rest.Config
+}
+
+//+kubebuilder:rbac:groups=benchmark.apecloud.io,resources=fios,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=benchmark.apecloud.io,resources=fios/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=benchmark.apecloud.io,resources=fios/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Fio object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
+func (r *FioReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	l := log.FromContext(ctx)
+
+	// TODO(user): your logic here
+	var fio benchmarkv1alpha1.Fio
+	if err := r.Get(ctx, req.NamespacedName, &fio); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	if fio.Status.Phase == benchmarkv1alpha1.Complete || fio.Status.Phase == benchmarkv1alpha1.Failed {
+		return intctrlutil.Reconciled()
+	}
+
+	jobs := NewFioJobs(&fio)
+
+	if fio.Status.Phase == "" {
+		l.Info("start fio", "fio", fio.Name)
+		fio.Status.Phase = benchmarkv1alpha1.Running
+		fio.Status.Total = len(jobs)
+	}
+
+	if fio.Status.Succeeded >= fio.Status.Total {
+		fio.Status.Phase = benchmarkv1alpha1.Complete
+	} else {
+		job := jobs[fio.Status.Succeeded]
+
+		existed, err := utils.IsJobExisted(r.Client, ctx, job.Name, fio.Namespace)
+		if err != nil {
+			l.Error(err, "failed to check if job exists", "job", job.Name)
+			return intctrlutil.RequeueWithError(err, l, "failed to check if job exists")
+		}
+
+		if !existed {
+			if err = controllerutil.SetOwnerReference(&fio, job, r.Scheme); err != nil {
+				l.Error(err, "failed to set owner reference for job", "job", job.Name)
+				return intctrlutil.RequeueWithError(err, l, "failed to set owner reference for job")
+			}
+
+			if err = r.Create(ctx, job); err != nil {
+				l.Error(err, "failed to create job", "job", job.Name)
+				return intctrlutil.RequeueWithError(err, l, "failed to create job")
+			}
+
+			// wait for the job to be created
+			l.Info("created job", "job", job.Name)
+			return intctrlutil.RequeueAfter(intctrlutil.RequeueDuration)
+		}
+
+		// check if the job is completed
+		status, err := utils.GetJobStatus(r.Client, ctx, job.Name, job.Namespace)
+		if err != nil {
+			l.Error(err, "failed to get job status", "job", job.Name)
+			return intctrlutil.RequeueWithError(err, l, "failed to get job status")
+		}
+
+		if status.Succeeded > 0 {
+			l.Info("job completed", "job", job.Name)
+			fio.Status.Succeeded++
+			// record the result
+			if err := utils.LogJobPodToCond(r.Client, r.RestConfig, ctx, job.Name, fio.Namespace, &fio.Status.Conditions, nil); err != nil {
+				return intctrlutil.RequeueWithError(err, l, "unable to record the log")
+			}
+		} else if status.Failed > 0 {
+			l.Info("job failed", "job", job.Name)
+			fio.Status.Phase = benchmarkv1alpha1.Failed
+		} else {
+			l.Info("job is running", "job", job.Name)
+		}
+	}
+
+	fio.Status.Completions = fmt.Sprintf("%d/%d", fio.Status.Succeeded, fio.Status.Total)
+	if err := r.Status().Update(ctx, &fio); err != nil {
+		return intctrlutil.RequeueWithError(err, l, "unable to update fio status")
+	}
+
+	return intctrlutil.RequeueAfter(intctrlutil.RequeueDuration)
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *FioReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&benchmarkv1alpha1.Fio{}).
+		Complete(r)
+}
